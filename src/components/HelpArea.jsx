bf84b047113aaccf6ee15d090814050bdfc9e6b1
ae12321-omen <80916217+ae12321@users.noreply.github.com>
2023-11-04 00:56:53 +0900

    fix [skip ci]

--- src/components/HelpArea.jsx
+++ src/components/HelpArea.jsx
@@ -8,7 +8,7 @@ const HELP_ITEMS = [
     text: 'Git is a version control system, helping you to manage your code and create code snapshots.',
   },
   //aaa
-  ///
+  /////
   {
     id: 'h2',
     title: 'What is GitHub?',
